d57cc3429de2379a8fd841ae5060e436e7c5bdbc
ridvanms <ridvans@abv.bg>
2024-11-21 13:03:06 +0200

    "Added Roadmap component to App, updated Heading component to accept 'tag' prop, and refactored Pricing component with new design elements and layout."

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,7 +1,42 @@
+import { smallSphere, stars } from "../assets";
+import Heading from "./Heading";
+import PricingList from "./PricingList";
 import { Section } from "./Section";
+import { LeftLine, RightLine } from "./design/Pricing";
 
 const Pricing = () => {
-  return <Section>Pricing</Section>;
+  return (
+    <Section className={`overflow-hidden`} id="pricing">
+      <div className="container relative z-2">
+        <div className="hidden relative justify-center mb-[6.5rem] lg:flex">
+          <img src={smallSphere} className="relative z-1" alt="smallSphere" />
+          <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
+            <img
+              src={stars}
+              width={950}
+              height={400}
+              className="w-full"
+              alt="stars"
+            />
+          </div>
+        </div>
+        <Heading tag="Get started with Brainwave" title="Pay one use forever" />
+        <div className="relative">
+          <PricingList />
+          <LeftLine />
+          <RightLine />
+        </div>
+        <div className="flex justify-center mt-10 text-n-2">
+          <a
+            href="#pricing"
+            className="text-xs font-code  font-bold tracking-wider uppercase border-b "
+          >
+            See the full details
+          </a>
+        </div>
+      </div>
+    </Section>
+  );
 };
 
 export default Pricing;
